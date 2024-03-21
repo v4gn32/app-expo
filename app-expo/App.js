fe67f0e2ff2244a8c173a53eb40773972417f47f
Vagner de Oliveira Florencio <113205762+v4gn32@users.noreply.github.com>
2024-03-21 12:51:41 -0300

    Add files via upload

---
+++ app-expo/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import HomePage from './src/pages/HomePage'
+
+const App = () => {
+
+return (
+
+<HomePage /> 
+
+);
+
+}
+
+export default App;
